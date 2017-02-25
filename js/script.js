a88a271ccd6b9f2b774c8efb0cb84dae0a3dab77
roe-el <rogar28@gmail.com>
2017-02-24 21:51:13 -0600

    Change 12 to 10 pages in link and add click for random page

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     	$(".searchResults").empty();
         var searchTerm = $(".searchText").val();
         console.log(searchTerm);
-        var link = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchTerm + "&srlimit=12&srqiprofile=classic&srprop=snippet&srenablerewrites=1";
+        var link = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchTerm + "&srlimit=10&srqiprofile=classic&srprop=snippet&srenablerewrites=1";
         console.log(link);
         $.ajax({
             url: link,
@@ -17,14 +17,14 @@ $(document).ready(function() {
                         var title = result.title;
                         var snippet = result.snippet;
                         snippet = snippet.split('.');
-                        $(".searchResults").append("<div class='well set'><a  href='" 
+                        $(".searchResults").append("<div class='well active set'><a  href='" 
                          + url 
                          + title
                          + "' target='_blank'><div class='heading'>" 
                          + title 
                          + "</div><div class='snippet'>" 
                          + snippet[0] 
-                         + "...</div></a></div").fadeIn();
+                         + "...</div></a></div>").fadeIn();
                     })
 
                 })
@@ -35,5 +35,8 @@ $(".searchText").keydown(function(event){
 	if(event.keyCode==13){
 		$(".go").trigger('click');
 	}
+});
+$(".random").click(function (){
+	window.open("http://en.wikipedia.org/wiki/Special:Random","_blank");
 });
 });
